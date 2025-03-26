d151be618fa874841258ee30d8b311ec742f115e
Edwin Carim Ramirez Vega <edcaramirezve@ittepic.edu.mx>
2025-03-26 00:21:02 +0000

    Paciente commit

--- register.php
+++ register.php
@@ -7,13 +7,13 @@
         $telefono=$_POST['telefono'];
         $edad=$_POST['edad'];
 
-        $checkUser="Select * from pacientes where nombre = '$usuario' and telefono = '$telefono'";
+        $checkUser="Select * from Pacientes where nombre = '$usuario' and telefono = '$telefono'";
         $result=$conn->query($checkUser);
         if($result->num_rows>0){
             echo "<script>alert('El usuario ya existe')</script>";
             print("Ya existe");
         }else{
-            $insertQuery="Insert into pacientes(nombre,telefono,edad,password) values('$usuario','$telefono','$edad','$password')";
+            $insertQuery="Insert into Pacientes(nombre,telefono,edad,password) values('$usuario','$telefono','$edad','$password')";
             if($conn->query($insertQuery)===TRUE){
                 echo "<script>alert('Usuario registrado')</script>";
                 print("Registrado");
@@ -30,7 +30,7 @@ if(isset($_POST['loginButton2'])){
     $usuario=$_POST['usuario'];
     $password=$_POST['password'];
 
-    $checkUser="Select * from pacientes where nombre = '$usuario' and password = '$password'";
+    $checkUser="Select * from Pacientes where nombre = '$usuario' and password = '$password'";
     $result=$conn->query($checkUser);
     if($result->num_rows>0){
         session_start();
